358070c5b4e3fe3c89701af4a6be64bfc9e1353e
NoNickle <zzhao1573@gmail.com>
2020-09-17 21:05:49 -0700

    Code Refactor

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.cluster import KMeans
-from ContentExtractor import contentExtractor
-from Preprocessor import preprocess
+from src.preprocessor import preprocess, textfilter, contentExtractor
 import numpy as np
 import jieba
 
+text_filter = textfilter.TextFilter(skip_word=[["对方正在使用", "收发消息"]],
+                                    skip_line="-------",
+                                    skip_prefix="): ")
 
-preprocessor = preprocess.Preprocessor()
+conversation = preprocess.Conversation(["(2017"], ["佩爱旗舰店"],
+                                       ["(2017", "佩爱旗舰店"], [],
+                                       ["-------", "佩爱旗舰店"], [],
+                                       text_filter)
 
 
 def tokenize(text):
@@ -16,28 +21,42 @@ def tokenize(text):
 
 
 def main():
-    preprocessor.transform()
+    f = open('chatbot.txt', 'r')
+    file_content = f.read()
+    text = file_content.splitlines()
+    stop_words = [" "]
+
+    conversation.transform(text)
 
     feature = FeatureUnion(
         transformer_list=[
+            ('questions', Pipeline([
+                ('selector', contentExtractor.ContentExtractor(0)),
+                ('Tf-Idf', TfidfVectorizer(stop_words=stop_words, tokenizer=tokenize, ngram_range=(1, 3)))
+            ])),
+            ('questionsPinYin', Pipeline([
+                ('selector', contentExtractor.ContentExtractor(2)),
+                ('Tf-Idf', TfidfVectorizer(stop_words=stop_words, ngram_range=(1, 5), analyzer=u'word'))
+            ])),
             ('answers', Pipeline([
                 ('selector', contentExtractor.ContentExtractor(1)),
-                ('Tf-Idf', TfidfVectorizer(stop_words=preprocessor.stopWords, tokenizer=tokenize, ngram_range=(1, 3)))
+                ('Tf-Idf', TfidfVectorizer(stop_words=stop_words, tokenizer=tokenize, ngram_range=(1, 3)))
             ])),
             ('answerPinyin', Pipeline([
                 ('selector', contentExtractor.ContentExtractor(3)),
-                ('Tf-Idf', TfidfVectorizer(stop_words=preprocessor.stopWords, ngram_range=(1, 5), analyzer=u'word'))
+                ('Tf-Idf', TfidfVectorizer(stop_words=stop_words, ngram_range=(1, 5), analyzer=u'word'))
             ]))
         ]
     )
-    features = feature.fit_transform(preprocessor.data)
+
+    features = feature.fit_transform(conversation.data)
     clusters = 50
     print(features.shape)
     print("Begin Clustering")
     k_means = np.array(KMeans(n_clusters=clusters).fit(features).labels_)
     print("Finished Clustering")
     classes = []
-    answers = [x[1] for x in preprocessor.data]
+    answers = [x[1] for x in conversation.data]
     for i in range(clusters):
         ind = np.where(k_means == i)[0]
         names = []
---
+++ src/preprocessor/textfilter.py
@@ -0,0 +1,70 @@
+from urlextract import URLExtract
+import unicodedata
+import re
+
+extractor = URLExtract()
+
+
+class TextFilter:
+
+    def __init__(self, skip_word=None, skip_line="", skip_prefix="",
+                 remove_punctuation=True, remove_number=True, remove_link=True):
+        if skip_word is None:
+            skip_word = []
+        self.text = None
+        self.keyword_skip_word = skip_word
+        self.keyword_skip_line = skip_line
+        self.keyword_skip_prefix = skip_prefix
+        self._remove_punctuation = remove_punctuation
+        self._remove_number = remove_number
+        self._remove_link = remove_link
+
+    @classmethod
+    def remove_link(cls, text) -> str:
+        urls = extractor.find_urls(text)
+        if len(urls) > 0:
+            for link in urls:
+                text = text.replace(link, "[链接]")
+        return text
+
+    @classmethod
+    def remove_numbers(cls, text) -> str:
+        nums = re.findall(r'-?\d+\.?\d*', text)
+        nums = sorted(nums, key=len, reverse=True)
+        if len(nums) > 0:
+            for num in nums:
+                text = text.replace(num, "[数字]")
+        return text
+
+    @classmethod
+    def skip_word(cls, text, words) -> str:
+        for word in words:
+            if type(word) is list:
+                assert len(word) > 1, "Too few keywords in list."
+                assert len(word) == 2, "Too many keywords in one skip."
+                if word[0] in text and word[1] in text:
+                    text = text.split(word[0])[0] + text.split(word[0])[1].split(word[1])[1]
+            elif type(word) is str:
+                assert len(word) > 0, "Cannot skip empty string."
+                if word in text:
+                    text = text.split(word)[0] + text.split(word)[1].split(word)[1]
+            else:
+                raise Exception('Unsupported skip words.')
+        return text
+
+    def fit_transform(self, text):
+        cur = unicodedata.normalize('NFKC', text)
+        if not self.keyword_skip_line != "" and self.keyword_skip_line in cur:
+            return ""
+        if len(self.keyword_skip_word) > 0:
+            cur = self.skip_word(cur, self.keyword_skip_word)
+        if self._remove_link:
+            cur = self.remove_link(cur)
+        if self._remove_number:
+            cur = self.remove_numbers(cur)
+        if self.keyword_skip_prefix != "" and self.keyword_skip_prefix in cur:
+            cur = cur.split(self.keyword_skip_prefix)[1]
+        if self._remove_punctuation:
+            cur = re.sub(r'''[][【】“”‘’"'、,.。:;@#?!&$/()%~`-―〈〉「」・@+_*=《》^…￥-]+\ *''',
+                         " ", cur, flags=re.VERBOSE)
+        return cur
